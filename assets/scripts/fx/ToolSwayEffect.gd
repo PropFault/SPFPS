87ab49fba62bc5f07492135292d6c67bfae1340c
Benedikt Brunner <benedikt.brunner@protonmail.com>
2022-07-18 17:23:13 +0200

    resolved conflict

--- assets/scripts/fx/ToolSwayEffect.gd
+++ assets/scripts/fx/ToolSwayEffect.gd
@@ -1,22 +1,17 @@
-
-
 extends Node
 export(NodePath) var _target
-export(NodePath) var _angularSource
+export(NodePath) var _source
 export(float) var effectMultiplier = 1.1
-export(float) var translateEffectMultiplier = 1.0
-export(float) var rotateEffectMultiplier = 1.0
-export(float) var locationSmoothing = 0.2
-export(float) var angularSmoothing = 0.2
-onready var angularSource = get_node(_angularSource)
+export(float) var angularEffectMultiplier = 1
+export(float) var smoothing = 0.2
 onready var target = get_node(_target)
+onready var source = get_node(_source)
 onready var baseOrigin = target.transform.origin
 var trailingVal = Vector3(0,0,0)
-var trailingBasis = Basis(Vector3(0,0,0),Vector3(0,0,0),Vector3(0,0,0))
-onready var baseOrientation = target.transform.basis
-var targetVal = Vector3(0,0,0)
-onready var lastRecOrigin = target.global_transform.origin
-onready var lastRecBasis = target.global_transform.basis
+onready var lastOrigin = source.global_transform.origin
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,18 +19,18 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _basis_subtract(a, b):
-	return Basis(a.x - b.x, a.y - b.y, a.z - b.z)
-func _basis_divide(a, b):
-	return Basis(a.x / b, a.y / b, a.z / b)
+
 func _process(delta):
-	#target.global_transform.origin = lastRecOrigin.linear_interpolate(target.global_transform.origin, 0.9)
-	target.global_transform.basis = lastRecBasis.slerp(target.global_transform.basis, 0.9).orthonormalized ( )
-	lastRecOrigin = target.global_transform.origin
-	lastRecBasis = target.global_transform.basis
+	var originDelta = source.global_transform.origin - target.global_transform.origin
+	var offsetOrigin = source.global_transform.origin + (-source.global_transform.basis.x - source.global_transform.basis.y - source.global_transform.basis.z).normalized() * originDelta.length() * angularEffectMultiplier
+	var origin = offsetOrigin
+	origin = Vector3(-origin.x, origin.y, -origin.z)
+	var targetVal = (origin - lastOrigin)/delta
+	trailingVal += (targetVal - trailingVal) / smoothing * delta
+	var parent = target
+	var add = parent.global_transform.basis.xform_inv(trailingVal) * (effectMultiplier - 1)
+	target.transform.origin = baseOrigin - add 
+	print(add)
+	lastOrigin = origin
 
 
-
-
-func _on_ent_player_velocityChanged(velocity):
-	targetVal = Vector3(-velocity.x, velocity.y, -velocity.z)
